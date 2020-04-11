b97e5d7e43a2507bb56f32ceaf7a0fbb21746009
Gabriel Todesco <gabriel.todesco97@gmail.com>
2020-04-11 02:43:40 -0300

    tratando rota inexistente e app component

--- src/router/index.js
+++ src/router/index.js
@@ -3,10 +3,16 @@ import VueRouter from 'vue-router'
 import Home from '../views/home/Home.vue'
 import Login from '../views/login/Login.vue'
 import Cadastro from '../views/cadastro/Cadastro.vue'
+import App from '../views/app/App.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
+  {
+    path: '*', // Caminho para quando for digitado uma url que não existe
+    name: 'Inexistent',
+    component: Home
+  },
   {
     path: '/',
     name: 'Home',
@@ -19,9 +25,14 @@ const routes = [
     // component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   },
   {
-    path: '/cadastrar',
+    path: '/cadastro',
     name: 'Cadastrar',
     component: Cadastro
+  },
+  {
+    path: '/app',
+    name: 'App',
+    component: App
   }
 ]
 
