80651eb6d7aec9c9d68927e88ebcd811a5aa93a9
Gabriel Todesco <gabriel.todesco97@gmail.com>
2020-04-18 01:13:34 -0300

    padrão de alert

--- src/mixins/mixinAlert.js
+++ src/mixins/mixinAlert.js
@@ -1,11 +1,11 @@
 const mixinAlert = {
     methods: {
 
-        async mxAlertConfirma() {
+        async mxAlertConfirma(mensagem) {
             const retorno = await this.$swal({
                 icon: 'warning',
-                title: '<strong style="font-family: Verdana, sans-serif">Tem certeza?</strong>',
-                html: '<p style="font-family: Verdana, sans-serif">Deseja continuar?</p>',
+                title: '<strong style="font-family: Verdana, sans-serif">Atenção</strong>',
+                html: '<p style="font-family: Verdana, sans-serif">' + mensagem + '</p>',
                 showCancelButton: true,
                 confirmButtonText: '<p style="font-family: Verdana, sans-serif">Sim</p>',
                 confirmButtonColor: '#3085d6',
@@ -16,6 +16,32 @@ const mixinAlert = {
                 preConfirm: () => {
                     return true
                   }
+            });
+
+            return retorno.value ? retorno.value : false;
+        },
+
+        async mxAlertErro(mensagem) {
+            const retorno = await this.$swal({
+                icon: 'error',
+                title: '<strong style="font-family: Verdana, sans-serif">Ooops...</strong>',
+                html: '<p style="font-family: Verdana, sans-serif">' + mensagem + '</p>',
+                showCancelButton: false,
+                confirmButtonText: '<p style="font-family: Verdana, sans-serif">OK</p>',
+                confirmButtonColor: '#d33',
+            });
+
+            return retorno.value ? retorno.value : false;
+        },
+
+        async mxAlertErroInesperado() {
+            const retorno = await this.$swal({
+                icon: 'error',
+                title: '<strong style="font-family: Verdana, sans-serif">x ERRO x</strong>',
+                html: '<p style="font-family: Verdana, sans-serif">Ocorreu um erro inesperado.</p>',
+                showCancelButton: false,
+                confirmButtonText: '<p style="font-family: Verdana, sans-serif">OK</p>',
+                confirmButtonColor: '#3085d6',
             });
 
             return retorno.value ? retorno.value : false;
