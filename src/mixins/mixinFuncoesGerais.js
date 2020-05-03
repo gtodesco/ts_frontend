14b84af8f90aebc893e71b8564f57b88a1bc57b5
Gabriel Todesco <gabriel.todesco97@gmail.com>
2020-05-03 02:19:44 -0300

    add mixin de conversão de data

--- src/mixins/mixinFuncoesGerais.js
+++ src/mixins/mixinFuncoesGerais.js
@@ -1,7 +1,20 @@
 const mixinFuncoesGerais = {
     methods: {
+
+        /**
+         * Método que direciona para uma rota passada por parâmetro
+         * @param {String} rota 
+         */
         mxIrPara: function (rota) {
             this.$router.push('/' + rota);
+        },
+
+        /**
+         * Método que retorna uma data no formado do banco de dados (date)
+         * @param {Date} data 
+         */
+        mxGetDataBd: function (data) {
+            return data.format('YYYY-MM-DD');
         },
     }
 }
