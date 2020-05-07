d636ccd53cdbeb24b18331d395265805e468350e
Gabriel Todesco <gabriel.todesco97@gmail.com>
2020-05-07 19:53:55 -0300

    título atividades

--- src/views/app/Atividades.vue
+++ src/views/app/Atividades.vue
@@ -1,7 +1,16 @@
 <template>
-  <div class="home">
-    Atividades
-  </div>
+  
+  <v-container>
+
+    <h1 class="headline">Atividades</h1>
+
+    <br>
+
+    
+
+
+  </v-container>
+
 </template>
 
 <script>
