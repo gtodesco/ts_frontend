66134542527a354b45dc654d3a6904d8ef07676d
Gabriel Todesco <gabriel.todesco97@gmail.com>
2020-05-07 19:56:23 -0300

    tela e listagem de tipos de atividade

--- src/views/app/TiposAtividade.vue
+++ src/views/app/TiposAtividade.vue
@@ -1,12 +1,147 @@
 <template>
-  <div class="home">
-    TiposAtividade
-  </div>
+  
+  <v-container>
+
+    <h1 class="headline">Tipos de Atividades</h1>
+
+    <br>
+
+    <v-row v-if="!sn_carregando_tipos_atividade">
+
+      <v-col
+        v-for="(tipo_atividade, i) in arrTiposAtividades"
+        :key="i"
+        cols="12"
+        md="4"
+      >
+        <v-card 
+          class="mx-auto"
+          outlined
+        >
+
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title class="headline mb-1">{{tipo_atividade.descricao}}</v-list-item-title>
+            </v-list-item-content>
+
+            <v-list-item-avatar
+              tile
+              size="80"
+              :color="tipo_atividade.color"
+            ></v-list-item-avatar>
+          </v-list-item>
+
+          <v-card-actions>
+            <v-tooltip bottom>
+              <template v-slot:activator="{ on }">
+                <v-btn
+                  icon
+                  dark
+                  large
+                  v-on="on"
+                >
+                  <v-icon color="primary">mdi-pencil-outline</v-icon>
+                </v-btn>
+              </template>
+              <span>Editar</span>
+            </v-tooltip>
+
+            <v-tooltip bottom>
+              <template v-slot:activator="{ on }">
+                <v-btn
+                  icon
+                  dark
+                  large
+                  v-on="on"
+                >
+                  <v-icon color="error">mdi-delete-forever-outline</v-icon>
+                </v-btn>
+              </template>
+              <span>Remover</span>
+            </v-tooltip>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+
+    <v-row v-if="sn_carregando_tipos_atividade" justify="center" align="center">
+      <v-progress-circular
+        v-if="sn_carregando_tipos_atividade"
+        :size="60"
+        color="primary"
+        indeterminate
+      ></v-progress-circular>
+    </v-row>
+
+    <v-tooltip top>
+      <template v-slot:activator="{ on }">
+        <v-btn
+          absolute
+          dark
+          fab
+          right
+          bottom
+          fixed 
+          x-large
+          v-on="on"
+          color="primary"
+          style="margin-bottom: 80px; margin-right: 20px;"
+        >
+          <v-icon>mdi-plus</v-icon>
+        </v-btn>
+      </template>
+      <span>Adicionar tipo de atividade</span>
+    </v-tooltip>
+
+  </v-container>
+
 </template>
 
 <script>
+import mixinAlert from '../../mixins/mixinAlert';
+import axios_ts from '../../axios-config';
 
 export default {
   name: 'TiposAtividade',
+
+  mixins: [
+    mixinAlert
+  ],
+
+  data: () => ({
+    
+    sn_carregando_tipos_atividade: false,
+
+    arrTiposAtividades: [],
+
+  }),
+
+  methods: {
+
+    async getDados() {
+
+      try {
+
+        this.sn_carregando_tipos_atividade = true;
+
+        const arrRetorno = await axios_ts.get('/tipo-atividade');
+
+        this.arrTiposAtividades = arrRetorno.data;
+
+        this.sn_carregando_tipos_atividade = false;
+
+      } catch (e) {
+
+        this.mxAlertErroInesperado(e);
+        this.sn_carregando_tipos_atividade = false;
+      }
+
+    },
+
+  },
+
+  async mounted() {
+    await this.getDados();
+  },
 }
 </script>
