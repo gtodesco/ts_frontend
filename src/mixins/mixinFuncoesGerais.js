4434f2113f8d882e28749bdbab8f4db05ca0618c
Gabriel Todesco <gabriel.todesco97@gmail.com>
2020-05-09 14:41:42 -0300

    formata string de tempo do banco para usar no front

--- src/mixins/mixinFuncoesGerais.js
+++ src/mixins/mixinFuncoesGerais.js
@@ -26,8 +26,8 @@ const mixinFuncoesGerais = {
         },
 
         /**
-         * Método que retorna uma data no formado do front
-         * @param {Date} data 
+         * Método que formata uma string de data do formato do banco para o formato do front
+         * @param {String} data 
          */
         mxFormataDataBd: function (data) {
 
@@ -38,7 +38,21 @@ const mixinFuncoesGerais = {
             const split = data.split('-');
 
             return split[2] + '/' + split[1] + '/' + split[0];
-        }
+        },
+
+        /**
+         * Método que formata uma string de tempo do formato do banco para o formato do front
+         * @param {String} time 
+         */
+        mxFormataTimeBd: function (time) {
+            if (time == null) {
+                return;
+            }
+
+            const split = time.split(':');
+
+            return split[0] + ':' + split[1];
+        },
     }
 }
 
