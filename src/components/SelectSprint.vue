8176c180836af3f7418d22df7a4a926b6577744a
Gabriel Todesco <gabriel.todesco97@gmail.com>
2020-05-11 19:21:43 -0300

    componente seleção de sprint

---
+++ src/components/SelectSprint.vue
@@ -0,0 +1,86 @@
+<template>
+    <v-select
+        v-model="sprint_selecionada"
+        :items="arrSprints"
+        :menu-props="{ offsetY: true }"
+        item-text="numero"
+        item-value="id"
+        clearable
+        label="Sprint"
+        :loading="sn_carregando_sprint"
+        @change="selecionaSprint()"
+    >
+    </v-select>
+</template>
+
+<script>
+import axios_ts from '../axios-config';
+import mixinAlert from '../mixins/mixinAlert';
+
+export default {
+    model: {
+        prop: 'sprint',
+        event: 'change-sprint'
+    },
+    name: 'SelectSprint', 
+
+    mixins: [
+        mixinAlert
+    ],
+
+    props: {
+        sprint: {
+            default: null
+        }
+    },
+    
+    data: () => ({
+        sn_carregando_sprint: false,
+
+        sprint_selecionada: null,
+
+        arrSprints: []
+    }),
+
+    methods: {
+
+        async getSprints() {
+            try {
+
+                this.sn_carregando_sprint = true;
+
+                const arrRetorno = await axios_ts.get('/sprint', {
+                    params: {
+                        equipe_id: localStorage.getItem('team')
+                    }
+                });
+
+                this.arrSprints = arrRetorno.data;
+
+                this.sn_carregando_sprint = false;
+
+            } catch (e) {
+
+                this.mxAlertErroInesperado(e);
+                this.sn_carregando_sprint = false;
+            }
+        },
+
+        selecionaSprint: function() {
+            this.$emit('change-sprint', this.sprint_selecionada != undefined ? this.sprint_selecionada : null);
+        }
+
+    },
+
+    async mounted() {
+        await this.getSprints();
+    },
+
+    watch: {
+        sprint: function(val) {
+            this.sprint_selecionada = val;
+        }
+    },
+
+}
+</script>
--- src/views/app/Impedimentos.vue
+++ src/views/app/Impedimentos.vue
@@ -1,12 +1,40 @@
 <template>
-  <div class="home">
-    Impedimentos
-  </div>
+  <v-container>
+    
+    <h1 class="headline">Impedimentos</h1>
+
+    <br>
+
+    <v-row>
+      <v-col 
+        cols="12"
+        sm="12"
+        md="4"
+      >
+        <SelectSprint
+          v-model="sprint_selecionada"
+        />
+
+      </v-col>
+    </v-row>
+
+
+  </v-container>
 </template>
 
 <script>
+import SelectSprint from '../../components/SelectSprint'; 
 
 export default {
   name: 'Impedimentos',
+  components: {
+    SelectSprint
+  },
+
+  data: () => ({
+
+    sprint_selecionada: null,
+    
+  }),
 }
 </script>
