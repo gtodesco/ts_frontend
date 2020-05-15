b2313b15cd660ace6dab94417077084da716d4be
Gabriel Todesco <gabriel.todesco97@gmail.com>
2020-05-15 11:47:53 -0300

    adiciona layout do gráfico

--- src/components/GraficoImpedimentos.vue
+++ src/components/GraficoImpedimentos.vue
@@ -9,13 +9,13 @@
     >
       <v-card>
         <v-toolbar dark color="primary">
-          <v-btn icon dark @click="fecharModal()">
-            <v-icon>mdi-close</v-icon>
-          </v-btn>
+            <v-spacer/>
+            <v-btn icon dark @click="fecharModal()">
+                <v-icon>mdi-close</v-icon>
+            </v-btn>
         </v-toolbar>
 
         <v-card-text>
-            
             <v-row v-if="sn_carregando_impedimentos" justify="center" align="center">
                 <v-progress-circular
                     v-if="sn_carregando_impedimentos"
@@ -25,10 +25,14 @@
                 ></v-progress-circular>
             </v-row>
             
-            <v-row v-if="!sn_carregando_impedimentos">
-                <!-- Grafico aqui -->
+            <v-row dense v-if="!sn_carregando_impedimentos">
+                <v-col cols="12">
+                    <canvas
+                        ref="grafico"
+                        style="max-height: 600px;"
+                    ></canvas>
+                </v-col>
             </v-row>
-
         </v-card-text>
       </v-card>
     </v-dialog>
@@ -39,6 +43,7 @@
 import mixinFuncoesGerais from '../mixins/mixinFuncoesGerais';
 // import axios_ts from '../axios-config';
 import mixinAlert from '../mixins/mixinAlert';
+import Chart from 'chart.js';
 
 export default {
     model: {    
@@ -75,6 +80,29 @@ export default {
             try {
 
                 this.sn_carregando_impedimentos = true;
+
+                this.chart = new Chart(this.$refs.grafico, {
+                    type: 'doughnut',
+                    data: {
+                        labels: ['Red', 'Blue', 'Yellow', 'Joao', 'Maria', 'Carlos', 'Millena'],
+                        datasets: [{
+                            data: [60, 20, 30, 15, 9, 35, 12],
+                            backgroundColor: ["#eb4c46", "#d68c58", "#ede266", "#acd658", "#6fd658", "#58d686", "#58d6b9",
+                            "#56a2c4", "#566cc4", "#3f3e70", "#b584f5", "#f584f1", "#d1c7d0", "#a7b6db","#465159", "#3c735f",
+                            "#4f4406", "#5c0519", "#a5c4ab", "#615c56", "#674175", "#d1b2b7", "#4b5447"],
+                            borderWidth: 5
+                        }],
+                    },
+                    options: {
+                        legend: {
+                            display: true,
+                            position: 'bottom'
+                        },
+                        maintainAspectRatio: false,
+                        aspectRatio: 1,
+                    }
+                });
+
                 this.sn_carregando_impedimentos = false;
 
             } catch (e) {
